3f71a971aec14c9d2ffe6b7f5adf5f1ed088bf4e
Mikhail Fadin <fadin.edu@gmail.com>
2025-11-07 00:17:11 +0200

    add user model migration, remove frontend test setup, and update dependencies

--- backend/app/db/models/user.py
+++ backend/app/db/models/user.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Boolean, Column, DateTime, String, func
+from sqlalchemy.dialects.postgresql.base import UUID
 
 from app.db import Base
 
@@ -6,6 +7,7 @@ from app.db import Base
 class User(Base):
     __tablename__ = "users"
 
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(
         DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
